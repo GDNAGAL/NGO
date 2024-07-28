373e21b37536aeb3823c81749d18294b38ef73a3
GD NAGAL <ganpatram@GORDHANs-MacBook-Air.local>
2024-07-28 15:37:20 +0530

    update

---
+++ products.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NGO</title>
+    <link rel="stylesheet" href="Assets/Bootstrap/Bootstrap.css">
+    <link rel="stylesheet" href="Assets/Custom/Style.css">
+    <script src="Assets/Bootstrap/Bootstrap.js"></script>
+    <script src="Assets/JQuery/jquery.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="contact.html">
+
+    <style>
+
+    </style>
+</head>
+<body>
+<?php require("include/header.php"); ?>
+<div style="background-color: #E6F3FF;">
+  <div class="container pt-4 pb-4">
+  <div class="row mb-3">
+      <div class="col-md-12">
+        <div class="bg-white shadow p-4 rounded-3 text-center">
+          <p>Your donation will help us move our cows to a permanent shelter so they can receive the love and medical attention they require.</p>
+          <strong>(This donation is under 80G exempted)</strong>
+          <p class="mb-0">Donate via Card, UPI, & Wallet (INR Only)</p>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-md-center">
+      <div class="col-md-8">
+        <!-- <div class="bg-white shadow p-4 rounded-3 mb-3"> -->
+          <h5>Products :</h5>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card p-3 mb-4 shadow">
+                <img src="https://www.doghomefoundation.com/wp-content/uploads/2024/05/hara-chara.jpg" alt="">
+                <h5>Green Fodder</h5>
+                <span style="font-size:12px">25 of 1000 Quantity Obtained.</span>
+                <div class="progress mt-1">
+                  <div class="progress-bar bg-success" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                </div>
+                <h6 class="mt-3">Rs 500/Green Fodder (100 KG)</h6>
+                <span style="font-size:12px">Add Items :</span>
+                <div class="d-flex justify-content-center align-items-center mt-2 mb-2">
+                  <div>
+                    <button class="btn btn-success shadow-none rounded-pill"><i class="bi bi-dash-lg"></i></button>
+                  </div>
+                  <div class="ms-3 me-3">0</div>
+                  <div>
+                    <button class="btn btn-success shadow-none rounded-pill"><i class="bi bi-plus-lg"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card p-3 mb-4 shadow">
+                <img src="https://www.doghomefoundation.com/wp-content/uploads/2024/05/Medical-Kit-copy.jpg" alt="">
+                <h5>Green Fodder</h5>
+                <span style="font-size:12px">25 of 1000 Quantity Obtained.</span>
+                <div class="progress mt-1">
+                  <div class="progress-bar bg-success" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                </div>
+                <h6 class="mt-3">Rs 500/Green Fodder (100 KG)</h6>
+                <span style="font-size:12px">Add Items :</span>
+                <div class="d-flex justify-content-center align-items-center mt-2 mb-2">
+                  <div>
+                    <button class="btn btn-success shadow-none rounded-pill"><i class="bi bi-dash-lg"></i></button>
+                  </div>
+                  <div class="ms-3 me-3">0</div>
+                  <div>
+                    <button class="btn btn-success shadow-none rounded-pill"><i class="bi bi-plus-lg"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card p-3 mb-4 shadow">
+              <img src="https://www.doghomefoundation.com/wp-content/uploads/2024/05/Medical-Kit-copy.jpg" alt="">
+              <h5>Green Fodder</h5>
+                <span style="font-size:12px">25 of 1000 Quantity Obtained.</span>
+                <div class="progress mt-1">
+                  <div class="progress-bar bg-success" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                </div>
+                <h6 class="mt-3">Rs 500/Green Fodder (100 KG)</h6>
+                <span style="font-size:12px">Add Items :</span>
+                <div class="d-flex justify-content-center align-items-center mt-2 mb-2">
+                  <div>
+                    <button class="btn btn-success shadow-none rounded-pill"><i class="bi bi-dash-lg"></i></button>
+                  </div>
+                  <div class="ms-3 me-3">0</div>
+                  <div>
+                    <button class="btn btn-success shadow-none rounded-pill"><i class="bi bi-plus-lg"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <!-- </div> -->
+      </div>
+      <div class="col-md-4">
+        <div class="bg-white shadow p-4 rounded-3">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div> 
+  <?php require("include/footer.php"); ?>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>                 
+  </body>
+</html>
